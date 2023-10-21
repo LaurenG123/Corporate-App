7c70377be580295fc0ac096b66b196ec030963e3
Lauren G <72687468+LaurenG123@users.noreply.github.com>
2023-10-21 12:48:24 +0100

    Add files via upload

---
+++ imports.py
@@ -0,0 +1,82 @@
+# Kivy core modules
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.image import Image
+from kivy.uix.popup import Popup
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.togglebutton import ToggleButton
+
+# Kivy graphics and canvas
+from kivy.graphics import Color, Rectangle, Line, Ellipse
+
+# Kivy properties
+from kivy.properties import NumericProperty, StringProperty, ListProperty
+
+# Kivy events
+from kivy.event import EventDispatcher
+
+# Kivy animation
+from kivy.animation import Animation
+
+# Kivy input events
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.behaviors import ToggleButtonBehavior
+
+# Kivy layouts
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.stacklayout import StackLayout
+
+# Kivy gestures
+from kivy.uix.scatter import Scatter
+#from kivy.uix.gestures import Gesture
+
+# Kivy clock
+from kivy.clock import Clock
+
+# Kivy sound
+from kivy.core.audio import SoundLoader
+
+# Kivy file chooser
+from kivy.uix.filechooser import FileChooserListView, FileChooserIconView
+
+# Kivy text markup
+#from kivy.uix.label import MarkupLabel
+
+# Kivy spinner
+from kivy.uix.spinner import Spinner
+
+# Kivy dropdown
+from kivy.uix.dropdown import DropDown
+
+# Kivy progress bar
+from kivy.uix.progressbar import ProgressBar
+
+# Kivy video player
+from kivy.uix.videoplayer import VideoPlayer
+
+# Kivy text editor
+from kivy.uix.codeinput import CodeInput
+
+# Kivy screen manager
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+# Kivy settings
+from kivy.config import Config
+
+
+
+from kivymd.app import MDApp
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.card import MDCard
+from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import MDScreen
+
+
+from kivy.uix.camera import Camera
